8a3b7a933d26de53661cf8ad1ff60a8c89ddd71f
Robbie Straw <drbawb@fatalsyntax.com>
2016-04-14 19:33:28 -0500

    (git) init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate sdl2;
+extern crate sdl2_ttf;
+
+fn main() {
+	println!("initalizing sdl ...");
+	let sdl_context = sdl2::init().ok().expect("could not init sdl2!?");
+
+	println!("let me tell you a story ...");
+    
+}
