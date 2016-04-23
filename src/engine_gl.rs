c6c0852137e6e6290e144ba6f2d7e8637c1bd556
Robbie Straw <drbawb@fatalsyntax.com>
2016-04-23 16:57:56 -0500

    get rid of regions and just adjust coordinates to screenspace
    (might as well let the GPU handle clipping off-screen verts for now.)

--- src/engine_gl.rs
+++ src/engine_gl.rs
@@ -37,17 +37,28 @@ struct ControlPath {
     pub samples: Vec<ControlPoint>,
 }
 
+/// Represents a mouse-input sample from some brush
+struct ControlPoint {
+    screen_xy: V2,
+}
+
 impl ControlPath {
-    pub fn new(context: &GlutinFacade, points: Vec<ControlPoint>) -> ControlPath {
+    pub fn new(context: &GlutinFacade, scanbox: V2, points: Vec<ControlPoint>) -> ControlPath {
         let mut vbuf_path = glium::VertexBuffer::empty_dynamic(context, points.len() * 6)
             .ok().expect("could not alloc vbuf");
 
+        let corrected_samples = points.iter().map(|point| {
+            let adj_x = (point.screen_xy.0 as f32 + (scanbox.0 as f32 / 2.0)) as i64;
+            let adj_y = (point.screen_xy.1 as f32 - (scanbox.1 as f32 / 2.0)) as i64;
+
+            ControlPoint { screen_xy: V2(adj_x, adj_y) }
+        }).collect();
 
         ControlPath {
             needs_render: true,
 
             buffer:  vbuf_path,
-            samples: points,
+            samples: corrected_samples,
         }
     }
 
@@ -66,7 +77,6 @@ impl ControlPath {
                 let adj_y = (point.screen_xy.1 as f32 / 360.0) * 1.0;
                 ( (adj_x - 1.0), -(adj_y - 1.0) )
             };
-
             writer.set(ofs + 0, Vert2 { pos: [        wx,        wy,   0.0], color: [0.75, 0.0, 0.5] });
             writer.set(ofs + 1, Vert2 { pos: [   wx+fudge,       wy,   0.0], color: [0.75, 0.0, 0.5] });
             writer.set(ofs + 2, Vert2 { pos: [         wx,  wy-fudge,  0.0], color: [0.75, 0.0, 0.5] });
@@ -78,21 +88,6 @@ impl ControlPath {
         }
 
         println!("final init offset: {}", ofs);
-    }
-}
-
-/// Represents a mouse-input sample from some brush
-struct ControlPoint {
-    screen_xy: V2,
-}
-
-struct Region {
-    pub paths: Vec<ControlPath>, // TODO: paths can span regions
-}
-
-impl Region {
-    pub fn new() -> Region {
-        Region { paths: vec![] }
     }
 }
 
@@ -174,7 +169,6 @@ impl Engine {
         let mut cursor_y = 0;
 
         // control point buffers
-        let mut regions = vec![Region::new()];
         let mut input_buffers: Vec<ControlPath>  = vec![];
         let mut input_samples: Vec<ControlPoint> = Vec::with_capacity(MAX_VERTS);
         let mut cursor_commit = true;
@@ -239,68 +233,6 @@ impl Engine {
             }
 
 
-            // alloc new region
-            let V2(rx,ry) = self.scanbox + V2(1280, 720);
-            let region_sqrt = (regions.len() as f64).sqrt();
-            if rx as f64 > (region_sqrt * 1280.0) || ry as f64 > (region_sqrt * 720.0){
-                println!("growing up/right");
-                let pitch = (region_sqrt + 1.0) as usize;
-                let next_square = pitch * pitch;
-
-                let mut buf = Vec::with_capacity(next_square);
-                mem::swap(&mut regions, &mut buf);
-                let mut old_drain = buf.into_iter();
-
-                for row in 0..pitch {
-                    for col in 0..pitch {
-                        if (row >= pitch - 1) || (col >= pitch - 1) {
-                            regions.push(Region::new());
-                        } else {
-                            regions.push(old_drain.next().expect("ran out of regions to copy during regrow !!!"));
-                        }
-                    }
-                }
-            }
-
-            if (self.scanbox.0 < 0) || (self.scanbox.1 < 0) {
-                println!("need to regrow left/down");
-                let pitch = (region_sqrt + 1.0) as usize;
-                let next_square = pitch * pitch;
-
-                let mut buf = Vec::with_capacity(next_square);
-                mem::swap(&mut regions, &mut buf);
-                let mut old_drain = buf.into_iter();
-
-                let mut idx = 0;
-                for row in 0..pitch {
-                    for col in 0..pitch {
-                        if (row == 0) || (col == 0) {
-                            println!("[{},{}] => new", row, col);
-                            regions.push(Region::new());
-                        } else {
-                            println!("[{},{}] => {}", row, col, idx);
-                            idx += 1;
-                            regions.push(old_drain.next().expect("ran out of regions to copy during regrow !!!"));
-                        }
-                    }
-                }
-
-                self.scanbox = V2(1280, 720) + self.scanbox;
-            }
-
-
-
-            // // copy old / allocate new
-            // for row in 0..pitch {
-            //     for col in 0..pitch {
-            //         if (row >= pitch - 1) || (col >= pitch - 1) {
-            //             regions.push(Region::new());
-            //         } else {
-            //             regions.push(old_drain.next().expect("ran out of regions to copy during regrow!"));
-            //         }
-            //     }
-            // }
-
             // handle cursor input
             //
             // we convert the control point to it's unit-position
@@ -309,63 +241,21 @@ impl Engine {
             let brush = self.brush;
             let V2(ofs_x, ofs_y) = self.scanbox;
 
-            let x1 = cursor_x as i64 + ofs_x;
-            let y1 = cursor_y as i64 + ofs_y;
-
-            let pitch = (regions.len() as f64).sqrt() as i64;
-            let col  = x1 / 1280;
-            let row  = y1 /  720;
-            let ridx = (row * pitch) + col; // row * 3rows/col + col
-
-            // blit in that region instead
-            //println!("[{}], row: {}, col: {}", ridx, row, col);
-            //println!("real ({},{})", x1, y1);
-            let x1 = match x1 > 1280 {
-                true  => x1 % (1280 * col+1),
-                false => x1,
-            };
-
-            let y1 = match y1 > 720 {
-                true  => y1 % ( 720 * row+1 ),
-                false => y1,
-            };
-            //println!("adj ({},{})", x1, y1);
-
-
             // store user input into control point buffer for the computed region
-            let (wx, wy) = Engine::world_to_unit(x1 as f64, y1 as f64);
             if cursor_down {
                 input_samples.push(ControlPoint {
-                    screen_xy: V2(x1 as i64, y1 as i64),
+                    screen_xy: V2(cursor_x as i64, cursor_y as i64),
                 });
             } else if !cursor_down && !cursor_commit {
                 // swap the input buffer with a fresh one
                 let mut input_buf = Vec::with_capacity(MAX_VERTS);
                 mem::swap(&mut input_samples, &mut input_buf);
 
-                // compute starting ridx
-                let pitch = (regions.len() as f64).sqrt() as i64;
-                let col  = input_buf[0].screen_xy.0 / 1280;
-                let row  = input_buf[0].screen_xy.1 /  720;
-                let ridx = (row * pitch) + col; // row * 3rows/col + col
-
-                // TODO: coordinate needs to be adjusted if it crosses regions as well
-                // commit dirty one to heap
-                println!("added {} points", input_buf.len());
-                let pathbuf = ControlPath::new(&self.context, input_buf);
-                regions[ridx as usize].paths.push(pathbuf);
+                let pathbuf = ControlPath::new(&self.context, self.scanbox, input_buf);
+                input_buffers.push(pathbuf);
                 cursor_commit = true;
             }
             
-            // if x1 + 5 < 1280 && x2 < 1280 && y1 < 720 && y2 + 10 < 720 {
-            //     // draw control point
-            // }
-
-            // let diffx = x2 - x1;
-            // let diffy = y2 - y1;
-            // println!("delta ({},{}), mag: {}", diffx, diffy, diffy-diffx);
-
-
             // composite frame
             let mut target = self.context.draw();
             target.clear_color(0.05, 0.05, 0.05, 1.0);
@@ -412,19 +302,6 @@ impl Engine {
                     .ok().expect("could not blit cursor example");
             }
 
-            // for each path draw control point there
-            for path in &mut input_buffers {
-                let path_uni = uniform! {
-                    ofs:   [0.0, 0.0, 0.0f32], 
-                    scale: 1.0f32,
-                };
-
-                // inflate each control point to six verts
-                path.draw();
-                target.draw(&path.buffer, &self.indices_tris, &self.path_program, &path_uni, &tri_params)
-                    .ok().expect("could not blit cursor example");
-            }
-
             // show frame time
             let mut time_ms = 0;
             let time = Instant::now().duration_since(frame_start_at);
@@ -435,8 +312,8 @@ impl Engine {
             // TODO: helper for this
             // strlen =>  (char width * text length) * scale
             let (hue_r, hue_g, hue_b) = self.color;
-            let buf_1 = format!("{}ms, # regions: {}, # drawn: {}, sb @ {:?}",
-                              time_ms, regions.len(), 0, self.scanbox);
+            let buf_1 = format!("{}ms, # paths: {}, # drawn: {}, sb @ {:?}",
+                              time_ms, input_buffers.len(), 0, self.scanbox);
 
             let buf_2 = format!("e = erase all, b = brush ({:?}), hue(i,o,p) => ({:02x},{:02x},{:02x})",
                                self.brush, hue_r, hue_g, hue_b);
@@ -464,7 +341,7 @@ impl Engine {
             text_blitter.draw(&buf_1[..], text_scale, (1.0 - strlen1, 1.0), &mut target);
             text_blitter.draw(&buf_2[..], text_scale, (1.0 - strlen2, 1.0 - strheight), &mut target);
 
-            self.draw_regions(&mut regions, &mut target);
+            self.draw_regions(&mut input_buffers, &mut target);
 
             target.finish()
                 .ok().expect("could not render frame");
@@ -479,53 +356,28 @@ impl Engine {
         }
     }
 
-    fn draw_regions(&mut self, regions: &mut Vec<Region>, target: &mut glium::Frame) {
+    fn draw_regions(&mut self, paths: &mut Vec<ControlPath>, target: &mut glium::Frame) {
         let V2(ofs_x, ofs_y) = self.scanbox;
         let top0   = ofs_y;
         let bot0   = ofs_y + 720;
         let left0  = ofs_x;
         let right0 = ofs_x + 1280;
 
-        let pitch = (regions.len() as f64).sqrt() as usize;
-
-        for row in 0..pitch {
-            for col in 0..pitch {
-                // (0,0), (0, 720)
-                let x = (col * 1280) as i64;
-                let y = (row *  720) as i64;
-                let ridx = col + (row * pitch);
-                println!("ridx: {}", ridx);
-
-                let top1   = y;
-                let bot1   = y + 720;
-                let left1  = x;
-                let right1 = x + 1280;
-
-                let in_scanbox = right1 >= left0 && left1 < right0
-                    && top1 < bot0 && bot1 >= top0;
-
-                if true {
-                    let wofs_x = (ofs_x as f32 / 1280.0);
-                    let wofs_y = (ofs_y as f32 /  720.0);
-
-                    let bofs_x = (col as f32 * 1280.0) / 1280.0;
-                    let bofs_y = (row as f32 *  720.0) /  720.0;
-                    println!("ofs: ({},{}), wofs: ({},{})", bofs_x, bofs_y, wofs_x, wofs_y);
-
-                    for path in &mut regions[ridx].paths {
-                        let path_uni = uniform! {
-                            ofs:   [-bofs_x + wofs_x, bofs_y - wofs_y, 0.0f32], // TODO: check why X needs to be inverted
-                            scale: 1.0f32,
-                        };
-
-                        // inflate each control point to six verts
-                        path.draw();
-                        target.draw(&path.buffer, &self.indices_tris, &self.path_program, &path_uni, &Default::default())
-                            .ok().expect("could not blit cursor example");
-                    }
-                }
-            }
+        let wofs_x = (ofs_x as f32 / 1280.0); // offset of the scanbox converted to the screen space unit square
+        let wofs_y = (ofs_y as f32 /  720.0); // offset of the scanbox converted to the screen space unit square
+        
+        for path in paths {
+            let path_uni = uniform! {
+                ofs:   [-wofs_x, -wofs_y, 0.0f32],
+                scale: 1.0f32,
+            };
+            
+            // inflate each control point to six verts
+            path.draw();
+            target.draw(&path.buffer, &self.indices_tris, &self.path_program, &path_uni, &Default::default())
+                .ok().expect("could not blit cursor example");
         }
+
     }
 
     fn world_to_unit(x: f64, y: f64) -> (f64, f64) {
